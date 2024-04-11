a7c5bba7ddda54c4f59e3d530cdf2ec1db996707
gustavo <gustavo.parfait@gmail.com>
2024-04-10 23:58:09 -0300

    100420242358NB

--- js/app.js
+++ js/app.js
@@ -84,7 +84,38 @@ function searchImages( parameter ) {
 
 function showImages( images ) {
 
-    console.log( images );
+    console.log(images)
+    cleanHTML();
+
+    images.forEach( image => {
+
+        const { previewURL, likes,views, largeImageURL } = image;
+
+        result.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white" >
+                <img class="w-full" src="${previewURL}">
+                <div class="p-4">
+                    <p class="font-bold">${likes} <span class="font-light">Me Gusta</span></p>
+                    <p class="font-bold">${views} <span class="font-light">Veces Vista</span></p>
+                    <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bolt text-center rounded mt-5 p-1" href="${largeImageURL}" target="_blank" rel="noopener noreferrer">Ver Imagen</a>
+                </div>
+            </div>
+        </div>
+        
+        `;
+    })
+}
+
+
+
+
+function cleanHTML() {
+
+    while( result.firstChild ) {
+
+        result.removeChild( result.firstChild );
+    }
 }
 
 
